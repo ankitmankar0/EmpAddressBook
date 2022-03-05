54dc30ef474fc46c8099b68b7dc1d57ad48a9368
ankitmankar0 <ankitmankar012@gmail.com>
2022-03-05 22:08:59 +0530

    Added Emp multiple Company and addressBook in One repo

---
+++ UC4 AddressBook/UC4 AddressBook/AddBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC4_AddressBook
+{
+    class AddBook
+    {
+        static void Main(string[] args)
+        {
+            AddrBook.GetCustomer();
+            AddrBook.Modify();
+            AddrBook.ListingPeople();
+            AddrBook.RemovePeople();
+
+        }
+    }
+}
