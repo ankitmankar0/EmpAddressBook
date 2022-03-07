dbbca5f8824f8b4caf2d37765e821319b0593570
ankitmankar0 <ankitmankar012@gmail.com>
2022-03-07 18:35:27 +0530

    New Files added

---
+++ UC3 AddressBook/UC3 AddressBook/AddBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC3_AddressBook
+{
+    class AddBook
+    {
+        static void Main(string[] args)
+        {
+            AddrBook.GetCustomer();
+                AddrBook.Modify();
+            AddrBook.ListingPeople();
+            // Sample.AddrBook.RemovePeople();
+
+        }
+    }
+}
